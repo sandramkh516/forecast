c004c62ba902aaab865e4ca8fa78987307407730
sandramkh516 <118845782+sandramkh516@users.noreply.github.com>
2023-01-19 01:23:01 +0330

    remove current

--- src/app.js
+++ src/app.js
@@ -115,38 +115,4 @@ function handleSubmit(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-function currentPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiKey = "3c9f615743fd9bf271obd0cc3t2aeaf2";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(currentCity);
-}
-function currentCity(response) {
-  // response.preventDefault();
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = response.data.city;
-  celsiustemp = response.data.temperature.current;
-  let temp = Math.round(celsiustemp);
-  let temperature = document.querySelector("#temp");
-  let skyDescription = document.querySelector("#sky");
-  let humidity = document.querySelector("#hum");
-  let wind = document.querySelector("#wind");
-  let feelsLike = document.querySelector("#feel");
-  let iconElement = document.querySelector("#icon");
-  let icon = response.data.condition.icon;
-  let iconUrl = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`;
-  let dateElement = document.querySelector("#date");
-  temperature.innerHTML = temp;
-  skyDescription.innerHTML = response.data.condition.description;
-  humidity.innerHTML = Math.round(response.data.temperature.humidity);
-  wind.innerHTML = Math.round(response.data.wind.speed);
-  feelsLike.innerHTML = Math.round(response.data.temperature.feels_like);
-  iconElement.setAttribute("src", iconUrl);
-  dateElement.innerHTML = formatDate();
-}
-navigator.geolocation.getCurrentPosition(currentPosition);
-let buttonCurrent = document.querySelector("#current-input");
-buttonCurrent.addEventListener("submit", currentPosition);
-
 search("Isfahan");
